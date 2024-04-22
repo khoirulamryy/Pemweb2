90d2ead63635a38449e3224245c5bb0e44566ac7
khoirulamryy <161549271+khoirulamryy@users.noreply.github.com>
2024-04-22 16:43:27 +0700

    Praktikum

---
+++ function.php
@@ -0,0 +1,28 @@
+<?php
+//function define by user
+
+
+//fungsi yang tidak mengembalikan nilai
+function salam (){
+    echo "assalamualaikum lek";
+}
+salam(); //cetaknya 
+
+echo "<br>";
+
+
+function panggil($arg1){
+    echo "bayar utang lek $arg1";
+
+}
+    panggil("irul");
+    echo "<br>";
+
+
+
+
+//fungsi yang mengembalikan nilai 
+
+
+
+?>
